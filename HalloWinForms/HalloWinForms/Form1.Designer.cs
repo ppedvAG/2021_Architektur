485ad279dc3da7c512f670c87d77b5744278899f
André <AndreR@outlook.de>
2021-12-06 14:40:00 +0100

    WinForms DPI aware

--- HalloWinForms/HalloWinForms/Form1.Designer.cs
+++ HalloWinForms/HalloWinForms/Form1.Designer.cs
@@ -37,58 +37,67 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(275, 81);
+            this.button1.AutoSize = true;
+            this.button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button1.Location = new System.Drawing.Point(230, 132);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(248, 71);
+            this.button1.Size = new System.Drawing.Size(237, 42);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Klick mich!";
+            this.button1.Tag = " ";
+            this.button1.Text = "Klick mich sehr fest!";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(112, 175);
+            this.label1.Location = new System.Drawing.Point(121, 224);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(90, 25);
+            this.label1.Size = new System.Drawing.Size(101, 32);
             this.label1.TabIndex = 1;
             this.label1.Text = "Binding:";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(146, 223);
+            this.textBox1.Location = new System.Drawing.Point(158, 285);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(309, 31);
+            this.textBox1.Size = new System.Drawing.Size(334, 39);
             this.textBox1.TabIndex = 2;
             // 
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(305, 29);
+            this.checkBox1.Location = new System.Drawing.Point(330, 37);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(150, 29);
+            this.checkBox1.Size = new System.Drawing.Size(159, 36);
             this.checkBox1.TabIndex = 3;
             this.checkBox1.Text = "checkBox1";
             this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(146, 276);
+            this.textBox2.Location = new System.Drawing.Point(158, 353);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(309, 31);
+            this.textBox2.Size = new System.Drawing.Size(334, 39);
             this.textBox2.TabIndex = 4;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Coral;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(758, 480);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Hallo WinForms";
             this.ResumeLayout(false);
