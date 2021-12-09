4412194369585ae6b871d2a77ddcb7c1e1bbe498
André <AndreR@outlook.de>
2021-12-09 10:18:11 +0100

    REST Client start

---
+++ GoogleBooksClientWPF/GoogleBooksClientWPF/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace GoogleBooksClientWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Suchen(object sender, RoutedEventArgs e)
+        {
+            var url = $"https://www.googleapis.com/books/v1/volumes?q={suchTb.Text}";
+            
+
+            
+        }
+    }
+}
