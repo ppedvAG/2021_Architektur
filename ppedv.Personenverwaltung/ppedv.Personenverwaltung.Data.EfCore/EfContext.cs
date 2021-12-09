552bc7de86a4def3b15cdfeaf7d31d49c35a0ec2
André <AndreR@outlook.de>
2021-12-09 14:33:31 +0100

    EfCore für Personenverwaltung

---
+++ ppedv.Personenverwaltung/ppedv.Personenverwaltung.Data.EfCore/EfContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ppedv.Personenverwaltung.Model;
+
+namespace ppedv.Personenverwaltung.Data.EfCore
+{
+    public class EfContext : DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Kunde> Kunden { get; set; }
+        public DbSet<Mitarbeiter> Mitarbeiter { get; set; }
+        public DbSet<Abteilung> Abteilungen { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=Personenverwaltung_DEV;Trusted_Connection=true;trustservercertificate=true");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Mitarbeiter>().ToTable("Mitarbeiter");
+            modelBuilder.Entity<Kunde>().ToTable("Kunden");
+            modelBuilder.Entity<Person>().ToTable("Personen");
+        }
+    }
+}
+
+
