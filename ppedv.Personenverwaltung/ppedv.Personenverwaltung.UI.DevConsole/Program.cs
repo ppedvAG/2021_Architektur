78009e877c9178161c39ce46b492192ecc6733c9
André <AndreR@outlook.de>
2021-12-09 16:40:25 +0100

    Bestes UI für Personenverwaltung

---
+++ ppedv.Personenverwaltung/ppedv.Personenverwaltung.UI.DevConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ppedv.Personenverwaltung.Logik;
+
+Console.WriteLine("Hello, World!");
+
+var core = new Core();
+
+var kunden = core.GetKundenThatHaveBirtdayThatMonth(1);
+
+foreach (var k in kunden)
+{
+    Console.WriteLine($"{k.Name}");
+}
+
