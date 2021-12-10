72293d44ef9c92cba6f80d71014782d7df7bbb2f
André <AndreR@outlook.de>
2021-12-10 14:20:46 +0100

    Blazor verunstaltet

---
+++ ppedv.Personenverwaltung/ppedv.Personenverwaltung.UI.Web.BlazorServer/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ppedv.Personenverwaltung.UI.Web.BlazorServer.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
