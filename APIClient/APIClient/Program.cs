864615ca4f85c1f2f964885dccdca84e482e71d7
André <AndreR@outlook.de>
2021-12-10 14:20:37 +0100

    Minimal API  + Client

---
+++ APIClient/APIClient/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using APIClient;
+
+Console.WriteLine("Hello, World!");
+
+
+var client = new swaggerClient("https://localhost:7163/", new HttpClient());
+
+var www = client.GetWeatherForecastAsync().Result;
+foreach (var item in www)
+{
+    Console.WriteLine(item.Summary);
+}
