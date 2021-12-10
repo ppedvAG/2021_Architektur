f453784681bd404fb6be1f9d30c503b6ff09f78c
André <AndreR@outlook.de>
2021-12-10 10:14:32 +0100

    DI per Hand und AutoFac

--- ppedv.Personenverwaltung/ppedv.Personenverwaltung.Logik/Core.cs
+++ ppedv.Personenverwaltung/ppedv.Personenverwaltung.Logik/Core.cs
@@ -5,11 +5,21 @@ namespace ppedv.Personenverwaltung.Logik
 {
     public class Core
     {
-        public IRepository Repository { get; } = new Data.EfCore.EfRepository();
+        public IRepository Repository { get; }
+
+        public Core(IRepository repo)
+        {
+            Repository = repo;
+        }
 
         public IEnumerable<Kunde> GetKundenThatHaveBirtdayThatMonth(int month)
         {
             return Repository.GetAll<Kunde>().Where(x => x.GebDatum.Month == month);
+        }
+
+        public Mitarbeiter GetMitarbeiterWithMostKunden()
+        {
+            return Repository.GetAll<Mitarbeiter>().OrderBy(x => x.Kunden.Count()).FirstOrDefault();
         }
     }
 }
--- ppedv.Personenverwaltung/ppedv.Personenverwaltung.UI.DevConsole/Program.cs
+++ ppedv.Personenverwaltung/ppedv.Personenverwaltung.UI.DevConsole/Program.cs
@@ -1,10 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Autofac;
+using ppedv.Personenverwaltung.Data.EfCore;
 using ppedv.Personenverwaltung.Logik;
 using ppedv.Personenverwaltung.Model;
+using ppedv.Personenverwaltung.Model.Contacts;
+using System.Reflection;
 
 Console.WriteLine("Hello, World!");
 
-var core = new Core();
+//DI direkt per Referenz
+//var core = new Core(new ppedv.Personenverwaltung.Data.EfCore.EfRepository());
+
+//DI per hand per Reflection
+//var path = @"C:\Users\Fred\source\repos\ppedvAG\2021_Architektur\ppedv.Personenverwaltung\ppedv.Personenverwaltung.Data.EfCore\bin\Debug\net6.0\ppedv.Personenverwaltung.Data.EfCore.dll";
+//var ass = Assembly.LoadFrom(path);
+//var typeMitDemRepo = ass.GetTypes().FirstOrDefault(x => x.GetInterfaces().Contains(typeof(IRepository)));
+//IRepository repo = (IRepository)Activator.CreateInstance(typeMitDemRepo);
+//var core = new Core(repo);
+
+//DI per AutoFac
+var builder = new ContainerBuilder();
+builder.RegisterType<EfRepository>().AsImplementedInterfaces();
+var container = builder.Build();
+
+var core = new Core(container.Resolve<IRepository>());
 
 var kunden = core.GetKundenThatHaveBirtdayThatMonth(1);
 
@@ -12,6 +31,7 @@ foreach (var k in kunden)
 {
     Console.WriteLine($"{k.Name}");
 }
+
 Console.WriteLine("Abteilungen");
 foreach (var abt in core.Repository.GetAll<Abteilung>())
 {
